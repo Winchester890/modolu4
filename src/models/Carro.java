e0b1386344ca01800a84a59ce269f6e2adefe9b1
Luiz Otavio <luizotav2005@gmail.com>
2023-03-07 11:38:40 -0300

    Tarefa modulo 4

---
+++ src/models/Carro.java
@@ -0,0 +1,94 @@
+package models;
+
+public class Carro {
+    private int quantidadePneus;
+    private int quantidadePortas;
+    private int quantidadeJanelas;
+    private int quantidadeFarois;
+    private int chassi;
+    private String cor;
+    private int combustivel;
+    private String arCondicionado;
+    private String motor;
+    private int ano;
+
+    public int getQuantidadePneus() {
+        return quantidadePneus;
+    }
+
+    public void setQuantidadePneus(int quantidadePneus) {
+        this.quantidadePneus = quantidadePneus;
+    }
+
+    public int getQuantidadePortas() {
+        return quantidadePortas;
+    }
+
+    public void setQuantidadePortas(int quantidadePortas) {
+        this.quantidadePortas = quantidadePortas;
+    }
+
+    public int getQuantidadeJanelas() {
+        return quantidadeJanelas;
+    }
+
+    public void setQuantidadeJanelas(int quantidadeJanelas) {
+        this.quantidadeJanelas = quantidadeJanelas;
+    }
+
+    public int getQuantidadeFarois() {
+        return quantidadeFarois;
+    }
+
+    public void setQuantidadeFarois(int quantidadeFarois) {
+        this.quantidadeFarois = quantidadeFarois;
+    }
+
+    public int getChassi() {
+        return chassi;
+    }
+
+    public void setChassi(int chassi) {
+        this.chassi = chassi;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public int getCombustivel() {
+        return combustivel;
+    }
+
+    public void setCombustivel(int combustivel) {
+        this.combustivel = combustivel;
+    }
+
+    public String getArCondicionado() {
+        return arCondicionado;
+    }
+
+    public void setArCondicionado(String arCondicionado) {
+        this.arCondicionado = arCondicionado;
+    }
+
+    public String getMotor() {
+        return motor;
+    }
+
+    public void setMotor(String motor) {
+        this.motor = motor;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+}
---
+++ src/models/Main.java
@@ -0,0 +1,57 @@
+package models;
+
+import models.Carro;
+
+import java.util.Scanner;
+public class Main {
+    public static void main (String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Qual é o número do chassi? ");
+        int chassi = scanner.nextInt();
+        System.out.print("Qual é o tipo de motor do carro? ");
+        String motor = scanner.nextLine();
+        System.out.print("Qual é a cor do carro? ");
+        String cor = scanner.nextLine();
+        System.out.print("O carro tem ar condicionado? " );
+        String arCondicionado = scanner.nextLine();
+        System.out.println("Qual é o ano de fabricação do carro? ");
+        int ano = scanner.nextInt();
+        System.out.print("Quantos litros o tanque do carro suporta? ");
+        int combustivel = scanner.nextInt();
+        System.out.print("Quantos pneus o carro tem? ");
+        int quantidadePneus = scanner.nextInt();
+        System.out.print("Quantas portas o carro tem? ");
+        int quantidadePortas = scanner.nextInt();
+        System.out.print("Quantas janelas o carro tem? ");
+        int quantidadeJanelas = scanner.nextInt();
+        System.out.print("Quantos faróis o carro tem? ");
+        int quantidadeFarois = scanner.nextInt();
+
+        Carro pecasCarro = new Carro();
+        pecasCarro.setChassi(chassi);
+        pecasCarro.setMotor(motor);
+        pecasCarro.setCor(cor);
+        pecasCarro.setCombustivel(combustivel);
+        pecasCarro.setQuantidadePneus(quantidadePneus);
+        pecasCarro.setQuantidadePortas(quantidadePortas);
+        pecasCarro.setQuantidadeJanelas(quantidadeJanelas);
+        pecasCarro.setQuantidadeFarois(quantidadeFarois);
+        pecasCarro.setArCondicionado(arCondicionado);
+        pecasCarro.setAno(ano);
+
+        System.out.println(" ");
+        System.out.println("---- Infos do carro ----");
+        System.out.println("Tipo de chassi: " + pecasCarro.getChassi());
+        System.out.println("Tipo de motor: " + pecasCarro.getMotor());
+        System.out.println("Cor: " + pecasCarro.getCor());
+        System.out.println("Litros do tanque de combustível: " + pecasCarro.getCombustivel());
+        System.out.println("Quantidade de pneus: " + pecasCarro.getQuantidadePneus());
+        System.out.println("Quantidade de portas: " + pecasCarro.getQuantidadePortas());
+        System.out.println("Quantidade de janelas: " + pecasCarro.getQuantidadeJanelas());
+        System.out.println("Quantidade de faróis: " + pecasCarro.getQuantidadeFarois());
+        System.out.println("Ano de fabricação: " + pecasCarro.getAno());
+        System.out.println("Ar condicionado: " + pecasCarro.getArCondicionado());
+
+    }
+}
